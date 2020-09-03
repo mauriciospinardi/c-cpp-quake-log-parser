b559db4db058257baa901fac1e9738a2898ad9c3
Maurício Spinardi <mauricio.spinardi@setis.com.br>
2020-09-03 19:26:43 -0300

    Add log.h

---
+++ src/log.h
@@ -0,0 +1,19 @@
+/**
+ * @file log.h
+ * @author Maurício Spinardi (mauricio.spinardi@gmail.com)
+ * @platform cygwin64
+ * @brief Namespace, class or interface definitions.
+ * @date 2020-09-03
+ * 
+ */
+
+#ifndef _LOG_H_INCLUDED_
+#define _LOG_H_INCLUDED_
+
+/********************/
+/* Public functions */
+/********************/
+
+/* TODO: */
+
+#endif /* #ifndef _LOG_H_INCLUDED_ */
