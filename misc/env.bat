4cec0bff2b8bc5a348b1ebbddd70dc504b80af0c
Maurício Spinardi <mauricio.spinardi@setis.com.br>
2020-09-03 12:22:47 -0300

    Add environment definition script

---
+++ misc/env.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+REM 
+REM @file env.bat
+REM @author Maurício Spinardi (mauricio.spinardi@gmail.com)
+REM @platform cygwin64
+REM @brief Development environment
+REM @date 2020-09-03
+REM 
+REM 
+
+:ENVIRONMENT
+
+SET ROOT_DIR=^
+C:\cygwin64
+
+SET PATH=^
+%ROOT_DIR%\bin;^
+C:\Program Files\Git\bin;
+
+EXIT /B 0
