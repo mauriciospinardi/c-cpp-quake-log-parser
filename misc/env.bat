3a4688b78e2517d0314c4b0f530d9d972b053c41
Maurício Spinardi <mauricio.spinardi@setis.com.br>
2020-09-03 13:17:01 -0300

    Update comments in ./misc/env.bat

--- misc/env.bat
+++ misc/env.bat
@@ -4,7 +4,7 @@ REM
 REM @file env.bat
 REM @author Maurício Spinardi (mauricio.spinardi@gmail.com)
 REM @platform cygwin64
-REM @brief Development environment
+REM @brief Auxiliary script. Defines the development environment.
 REM @date 2020-09-03
 REM 
 REM 
