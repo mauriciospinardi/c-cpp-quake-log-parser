d190fb41190371db992d04974d3dc679b8f0d814
Maurício Spinardi <mauricio.spinardi@setis.com.br>
2020-09-03 13:17:29 -0300

    Add main.c

---
+++ src/main.c
@@ -0,0 +1,32 @@
+/**
+ * @file main.c
+ * @author Maurício Spinardi (mauricio.spinardi@gmail.com)
+ * @platform cygwin64
+ * @brief Ponto de entrada.
+ * @date 2020-09-03
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**************************/
+/* Funções externalizadas */
+/**************************/
+
+/**
+ * @brief Ponto de entrada.
+ * 
+ * @param[in] argc contador de argumentos
+ * @param[in] argv vetor de argumentos
+ * 
+ * @return int EXIT_xxx
+ * 
+ */
+int main(int argc, char *argv[])
+{
+    (void) argc;
+    (void) argv;
+
+    return EXIT_SUCCESS;
+}
