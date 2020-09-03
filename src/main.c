5220a7d0ce3ef6d4e44b151475259565a1c5344b
Maurício Spinardi <mauricio.spinardi@setis.com.br>
2020-09-03 19:25:28 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,31 +2,81 @@
  * @file main.c
  * @author Maurício Spinardi (mauricio.spinardi@gmail.com)
  * @platform cygwin64
- * @brief Ponto de entrada.
+ * @brief Entry point.
  * @date 2020-09-03
  * 
  */
 
+#include "parser.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
-/**************************/
-/* Funções externalizadas */
-/**************************/
+/***********************/
+/* Function prototypes */
+/***********************/
+
+static void
+terminate(const char *executable, int error);
+
+/********************/
+/* Public functions */
+/********************/
 
 /**
- * @brief Ponto de entrada.
+ * @brief Entry point.
  * 
- * @param[in] argc contador de argumentos
- * @param[in] argv vetor de argumentos
+ * @param[in] argc argument count
+ * @param[in] argv argument vector
  * 
  * @return int EXIT_xxx
  * 
  */
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    (void) argv;
+    PARSER_TRACE("argc [%d] argc[0] [%s]", argc, argv[0]);
 
-    return EXIT_SUCCESS;
+    if (argc <= 1)
+    {
+        terminate(argv[0], -1);
+    }
+
+    /* TODO: code */
+
+    terminate(argv[0], 0);
+
+    return EXIT_FAILURE; /* Should never hit this line! */
+}
+
+/*********************/
+/* Private functions */
+/*********************/
+
+/**
+ * @description Show help and terminates execution.
+ * 
+ * @param[in] executable current executable path (can be relative)
+ * @param[in] error autodescriptive
+ */
+static void
+terminate(const char *executable, int error)
+{
+    printf("\nUsage: %s [file]", executable);
+    printf("\n");
+
+    /* TODO: error translation */
+
+    /* (ex.)
+     * printf("\n    Error: [file] is missing");
+     * printf("\n")
+     */
+
+    if (!error)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        exit(EXIT_FAILURE);
+    }
 }
